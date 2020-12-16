300bf705ea2117f2948d6b1f8a19a4213519322d
Jose Rodriguez <jrodriguezballester@gmail.com>
2020-12-16 17:16:03 +0100

    Login completo

--- app/src/main/java/com/example/farmacia2/LoginActivity.java
+++ app/src/main/java/com/example/farmacia2/LoginActivity.java
@@ -1,8 +1,8 @@
 package com.example.farmacia2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
@@ -12,48 +12,61 @@ import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class LoginActivity extends AppCompatActivity {
     private Button btnLogin;
     private EditText editTextEmail;
     private EditText editTextPassword;
     private Switch switchRemember;
+    private SharedPreferences preferences;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // crear el archivo preferences
+        preferences = getSharedPreferences("preferences", Context.MODE_PRIVATE);
+
+
         // asociar Valores interface Grafica
         bindUI();
+        // recoger valores del preferences
+        setCredencialsIfExists();
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 String email = editTextEmail.getText().toString();
                 String pass = editTextPassword.getText().toString();
-                boolean logeado=login(email,pass);
-               if (logeado){
-                   goToMain();
-               }
+                boolean logeado = login(email, pass);
+                if (logeado) {
+                    goToMain();
+                    if (switchRemember.isChecked()) {
+                        saveOnPreferences(email, pass);
+                    }
+                }
 
             }
         });
     }
 
 
-
     /**
      * Asociar variables Java a Elementos Interface Grafica
      */
-    private void bindUI(){
-        btnLogin=findViewById(R.id.buttonLogin);
-        editTextEmail =findViewById(R.id.editTextEmail);
-        editTextPassword=findViewById(R.id.editTextPassword);
-        switchRemember =findViewById(R.id.switchRemember);
+    private void bindUI() {
+        btnLogin = findViewById(R.id.buttonLogin);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        switchRemember = findViewById(R.id.switchRemember);
     }
 
     /**
      * Devuelve True si el parametro no esta vacio y cumple la expresion regular de Email
+     *
      * @param email
      * @return
      */
@@ -63,6 +76,7 @@ public class LoginActivity extends AppCompatActivity {
 
     /**
      * Devuelve True si tiene mas de 4 caracteres
+     *
      * @param password
      * @return
      */
@@ -72,6 +86,7 @@ public class LoginActivity extends AppCompatActivity {
 
     /**
      * Devuelve True si email y password son validados; sino muestra mensajes de error
+     *
      * @param email
      * @param pass
      * @return
@@ -97,4 +112,50 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    /**
+     * Guardar email,pass en SharePreferences;
+     *
+     * @param email
+     * @param pass
+     */
+    private void saveOnPreferences(String email, String pass) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("EMAIL", email);
+        editor.putString("PASS", pass);
+        // editor.commit(); // [bloqueante y síncrono]
+        editor.apply(); // [no bloqueante y asíncrono]
+    }
+
+    /**
+     * Rellena los editText con los valores de preferences
+     */
+    private void setCredencialsIfExists() {
+        String email = getEmailPreferences();
+        String password = getPasswordPreferences();
+        //  Toast.makeText(this, "e"+email+"-"+password, Toast.LENGTH_SHORT).show();
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            editTextEmail.setText(email);
+            editTextPassword.setText(password);
+        }
+
+    }
+
+    /**
+     * Obtener el Email guardado en SharePreferences
+     *
+     * @return
+     */
+    private String getEmailPreferences() {
+        return preferences.getString("EMAIL", "");
+    }
+
+    /**
+     * Obtener el Password guardado en SharePreferences
+     *
+     * @return
+     */
+    private String getPasswordPreferences() {
+        return preferences.getString("PASS", "");
+    }
 }
+
--- app/src/main/java/com/example/farmacia2/MainActivity.java
+++ app/src/main/java/com/example/farmacia2/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.farmacia2;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        preferences = getSharedPreferences("preferences", Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_logout:
+logOut();
+               return true;
+            case R.id.menu_forget:
+                removeSharePreferences();
+                logOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    /**
+     * Borrar el contenido del preferences
+     */
+    private void removeSharePreferences() {
+        preferences.edit().clear().apply();
+    }
+
+
+    /**
+     * Vamos al loginActivity pero como una nueva tarea
+     */
+    private void logOut() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
